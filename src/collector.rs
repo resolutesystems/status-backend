c7ed4425a6c22f652bbcfa6eddbda89878ace694
Hito <dkuranda@pm.me>
2024-05-28 14:56:45 +0200

    updated collector

--- src/collector.rs
+++ src/collector.rs
@@ -1,5 +1,4 @@
 // collector is a thread (entirely separate from axum) which gathers system data and saves it into redis
-// (code needs to be cleaned A LOT)
 
 use byte_unit::{Byte, Unit};
 use sqlx::PgPool;
