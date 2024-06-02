b3f065920d8b950eeaa47b9cca2e4996094e806d
HitoIRL <hitoirl1@gmail.com>
2024-06-02 03:37:52 +0200

    it wasnt fixed :3

--- src/routes.rs
+++ src/routes.rs
@@ -7,7 +7,7 @@ use crate::{models::{DataPoints, Incident}, AppContext};
 // TODO(hito): cache responses
 
 pub async fn datapoints(ctx: Extension<AppContext>) -> Json<Vec<DataPoints>> {
-    let datapoints = sqlx::query_as!(DataPoints, "SELECT * FROM datapoints ORDER BY id DESC LIMIT 5")
+    let datapoints = sqlx::query_as!(DataPoints, "SELECT * FROM (SELECT * FROM datapoints ORDER BY id DESC LIMIT 5) subquery ORDER BY id")
         .fetch_all(&ctx.db)
         .await
         .unwrap();
